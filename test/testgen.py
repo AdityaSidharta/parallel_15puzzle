745f57de8a75146070e3d8d70bcd4c5417669211
alexunxus <b01401089@ntu.edu.tw>
2021-12-19 21:19:57 -0500

    modify testgen.py making cases not too hard

--- test/testgen.py
+++ test/testgen.py
@@ -1,4 +1,22 @@
 import random
+import numpy as np
+
+dirs = [-1,0,1,0,-1]
+
+def swapzero(step, n):
+    arr = np.array([i for i in range(n*n)])
+    x , y = 0, 0
+    for _ in range(step):
+        d = random.randint(0,3)
+        dx = x + dirs[d]
+        dy = y + dirs[d+1]
+        if dx >= 0 and dy >= 0 and dx < n and dy < n:
+            tmp = arr[x*n+y]
+            arr[x*n+y] = arr[dx*n+dy]
+            arr[dx*n+dy] = tmp
+            x = dx
+            y = dy
+    return arr
 
 
 if __name__ == '__main__':
@@ -11,9 +29,8 @@ if __name__ == '__main__':
         for i in range(case_num):
             size = 4 # random.randint(2,4)
             f.write(f"{size}\n")
-            l = [i for i in range(size*size)]
-            random.shuffle(l)
-            for i in range(len(l)):
+            l = swapzero(80, size)
+            for i in range(l.shape[0]):
                 if (i+1) % size == 0:
                     f.write(f"{l[i]}\n")
                 else:
